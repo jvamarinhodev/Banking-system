85dd9ba41f65b326ffbd05a8a89830f4a3926910
Hypeeer <vittorjoao18@hotmail.com>
2026-02-24 23:53:27 -0300

    refactor(ui): the loop checks if the user exists.

--- src/services/createAccount.js
+++ src/services/createAccount.js
@@ -2,15 +2,15 @@ import fs from 'fs/promises';
 import path from 'path';
 //absolute path
 const accountPath = path.resolve('src', 'repositories');
-//throws invalid name error 
+//throws invalid name error
 export default async function buildAccount(accountName) {
-  if(!accountName || !accountName.trim()){
+  if (!accountName || !accountName.trim()) {
     throw new Error('Account name cannot be empty');
-  };
+  }
 
-  const safeName = accountName.replace(/[<>:"/\\|?*]/g, '') // valid characters 
+  const safeName = accountName.replace(/[<>:"/\\|?*]/g, ''); // valid characters
 
-  await fs.mkdir(accountPath, {recursive: true})//creates and validates if it exists
+  await fs.mkdir(accountPath, { recursive: true }); //creates and validates if it exists
 
   const filePath = path.join(accountPath, `${safeName}.json`);
 
@@ -18,8 +18,7 @@ export default async function buildAccount(accountName) {
     await fs.access(filePath); // If it exists, it doesn't throw an error, if it exists, it throws an error.
     throw new Error('Account already exists');
   } catch (err) {
-
-    if(err.code !== 'ENOENT'){
+    if (err.code !== 'ENOENT') {
       throw err;
     }
 
@@ -28,6 +27,6 @@ export default async function buildAccount(accountName) {
       balance: 0,
     };
     await fs.writeFile(filePath, JSON.stringify(data, null, 2));
-  return true
+    return true;
   }
-};
+}
--- src/ui/createAccount.ui.js
+++ src/ui/createAccount.ui.js
@@ -1,43 +1,53 @@
-import chalk from "chalk";
-import inquirer from "inquirer";
+import chalk from 'chalk';
+import inquirer from 'inquirer';
 //service
-import buildAccount from "../services/createAccount.js";
+import buildAccount from '../services/createAccount.js';
 
 export const createAccount = async () => {
-    console.log(chalk.bgGreen.black('Congratulations on choosing our bank.'));
-    console.log(chalk.green('Please set your account options below.'));
-    await requestNameAccount()
-}
+  console.log(chalk.bgGreen.black('Congratulations on choosing our bank.'));
+  console.log(chalk.green('Please set your account options below.'));
+  await requestNameAccount();
+};
 
 const requestNameAccount = async () => {
-  try {
+  let existsUser = false;
+  while (!existsUser) {
+    try {
+      const answerUser = await inquirer.prompt([
+        {
+          name: 'accountName',
+          message: 'Enter a name for your account:',
 
-    const answerUser = await inquirer.prompt([
-      {
-        name: "accountName",
-        message: "Enter a name for your account:",
-        
-        validate: input => {
-          if (!input.trim()) {
-            return "Account name cannot be empty";
-          }
+          validate: (input) => {
+            if (!input.trim()) {
+              return 'Account name cannot be empty';
+            }
 
-          if (/[\\/:*?"<>|]/.test(input)) {
-            return "Name contains invalid characters";
-          }
+            if (/[\\/:*?"<>|]/.test(input)) {
+              return 'Name contains invalid characters';
+            }
 
-          return true;
-        }
+            return true;
+          },
+        },
+      ]);
+
+      const accountName = answerUser.accountName.trim();
+
+      const errExists = await buildAccount(accountName);
+      // if the user error exists, it returns to the prompt to type again
+      if (errExists === false) {
+        existsUser = true;
+      } else {
+        successMessage(accountName);
+        break;
       }
-    ]);
-
-    const accountName = answerUser.accountName.trim();
-
-    await buildAccount(accountName);
-
-    successMessage(accountName);
-
-  } catch (err) {
-    console.log(err.message);
+    } catch (err) {
+      console.log(err.message);
+    }
   }
+};
+
+const successMessage = (accountName) => {
+  console.log(chalk.green(`Congratulations Mr(s) ${accountName} your account has been created.`));
 };
